7bd6fca76716985fb241811d38ed0dd716217f76
Laila <113842975+Lailamrfh@users.noreply.github.com>
2023-11-17 17:17:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//* Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//* Ketika hamburger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
